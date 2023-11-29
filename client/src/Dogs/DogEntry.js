fc5659b99f65106b51533c93a411ba056f7063ca
Ryan Ramsey <rramsey1115@gmail.com>
2023-11-29 08:39:36 -0600

    buttons added to all dogs

--- client/src/Dogs/DogEntry.js
+++ client/src/Dogs/DogEntry.js
@@ -3,6 +3,7 @@ export const DogEntry = ({currentDog}) => {
         <section className="dog-entry">
             <img className="dog-entry-img" src={currentDog.picUrl} alt="current dog"/>
             <h4 className="dog-entry-name">{currentDog.name}</h4>
+            <button className="remove-button">Remove - </button>
         </section>
     )
 }
