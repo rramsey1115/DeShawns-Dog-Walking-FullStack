b8d1122c81f262d88989c35a7e50fec2a3d0dfef
Ryan Ramsey <rramsey1115@gmail.com>
2023-11-30 15:33:25 -0600

    so I don't lose my form - still not working

--- client/src/Forms/EditWalkerForm.js
+++ client/src/Forms/EditWalkerForm.js
@@ -3,36 +3,61 @@ import { useEffect, useState } from "react"
 import { getAllCities } from "../apiManager"
 import "./EditWalker.css"
 
-export const EditWalkerForm = ({walker}) => {
+export const EditWalkerForm = ({ walker }) => {
     const [allCities, setAllCities] = useState([]);
+    const [walkerCities, setWalkerCities] = useState([]);
+    const [updatedWalker, setUpdatedWalker] = useState({
+        id: walker.id,
+        name: walker.name,
+        about: walker.about,
+        picUrl:walker.picUrl,
+        cities: walker.cities
+    });
+
+    const findAndSetWalkerCities = () => {
+        let res = [];
+        walker.cities?.map(wc => res.push(wc));
+        setWalkerCities(res);
+    }
+
     const getAndSetAllCities = () => {
         getAllCities().then(data => setAllCities(data));
     }  
+
+    useEffect(()=>{
+        findAndSetWalkerCities();
+    },[walker, updatedWalker]);
+
     useEffect(()=>{
         getAndSetAllCities();
+    },[walker])
+
+    useEffect(()=>{
+        setUpdatedWalker(walker);
     },[walker])
 
     return (
     <form className="walker-edit-form">
         <div className="form-div">
             <label>Name: {" "}
-                <input className="walker-edit-name" type="text" name="name"/>
-            </label>
-        </div>
-        <div className="form-div">
-            <label>About: {" "}
-                <input className="walker-edit-about" type="text" name="about"/>
+                <input className="walker-edit-name" type="text" defaultValue={walker?.name}/>
             </label>
         </div>
         <div className="form-div">
             <label>Picture URL: {" "}
-                <input className="walker-edit-pic" type="text" name="pic"/>
+                <input className="walker-edit-pic" type="text" defaultValue={walker?.picUrl}/>
             </label>
         </div>
+        <div className="form-div">
+            <label>About: {" "}
+                <textarea className="walker-edit-about" type="text" defaultValue={walker?.about}/>
+            </label>
+        </div>
+
         {/* defaultChecks boxes which match the walker's cities */}
         {allCities.map(city => {
             let match = false;
-            walker.cities?.map(wc => {
+            walkerCities?.map(wc => {
                 if (city.id === wc.id) 
                 {
                    match = true;
@@ -50,7 +75,29 @@ export const EditWalkerForm = ({walker}) => {
                     </label>
                 </div>
             )
-        })}   
+        })}
+
+
 
     </form>)
 }
+
+
+
+/* This is may be a shorter way of doing things, but doesn't work YET...
+{allCities.map(city => {
+            let match = false;
+            walker.cities?.map(wc => {
+                if (city.id === wc.id) 
+                {
+                   match = true;
+                }
+            })
+            return (
+            <div className="form-div" key={city.id}>
+                <label>
+                    <input type="checkbox" defaultChecked={match===true ? true : false} value={city.id}/>{city.name}
+                </label>
+            </div>
+            )
+        })} */
